aa57bc269b1722309dfd978c8696b67eded4c3db
Colton Marschik <colton.marschik@gmail.com>
2019-02-19 17:31:27 -0700

    Added more lambda work

--- section_twelve_pycharm_three_six_ap/lambdas.py
+++ section_twelve_pycharm_three_six_ap/lambdas.py
@@ -1,13 +1,14 @@
-l = lambda x:x > 5 #l is a lambda function that takes in a parameter(x) & returs wheter x > 5
-
-# def l(x):
-#     return x > 5
-# ^same as lambda x:x > 5
-
-
 def alter(values, check):
+    # return list(filter(check,values)) #same as below
     return [val for val in values if check(val)]
 
-my_list = [1,2,3,4,5]
-another_list = alter(my_list, lambda x: x != 5)
-print(another_list)
+
+def remove_numbers(value):
+    return alter(value, lambda x: x not in [str(n) for n in range(10)]) #returns true (returns as a string) if character is int in range 10
+
+
+def skip_letters(value, letter):
+    return alter(value, lambda x: x != letter)
+
+print(remove_numbers("hel5lo"))
+print(skip_letters("hello", "e"))
